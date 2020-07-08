9224f4add178f7d61374fb75f4c9b665e2d657e4
1Bitcoin <shigalkin@mail.ru>
2020-07-08 23:19:21 +0300

    add task 3 (ragexp)

--- ragexp/ragexp/index.php
+++ ragexp/ragexp/index.php
@@ -1,5 +1,48 @@
 <?php
 
-echo "Hello World!";
+$regexp = "!^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$!";
+
+function parseInfo($regexp, $word)
+{
+    if (preg_match($regexp, $word))
+        echo "+ found word '{$word}'\n";
+    else
+        echo "- nothing found\n";
+}
+
+function parseFile($descriptor, $regexp)
+{
+    if ($descriptor)
+    {
+        while (($word = fgets($descriptor)) !== false)
+        {
+            $word = substr($word, 0, -2);
+            parseInfo($regexp, $word);
+        }
+
+        if (!feof($descriptor))
+            echo "Error fgets()\n";
+    }
+    else
+        echo "Error while open file\n";
+}
+
+function closeFiles($firstDescription, $secondDescription)
+{
+    if ($firstDescription)
+        fclose($firstDescription);
+
+    if ($secondDescription)
+        fclose($secondDescription);
+}
+
+$correctNumbers = fopen("correctNumbers.txt", "r");
+$uncorrectNumbers = fopen("uncorrectNumbers.txt", "r");
+
+parseFile($correctNumbers, $regexp);
+parseFile($uncorrectNumbers, $regexp);
+
+closeFiles($correctNumbers, $uncorrectNumbers);
+
 
 ?>
